7a20370d5bd7011d6b9c496e4299e453274e748a
SerraSilvia <silvia.serra.7e6@itb.cat>
2026-02-02 16:04:42 +0100

    Add Vue Router configuration with initial routes

---
+++ frontend/src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/Home.vue')
+    },
+         {
+      path: "/login",
+      name: "login",
+      component: () => import('@/views/Login.vue'),
+      props: true
+    },
+    {
+      path: '/ofertas',
+      name: 'ofertas',
+      component: () => import('@/views/Ofertas.vue'),
+    },
+    {
+      path: "/oferta/:id",
+      name: "oferta",
+      component: () => import('@/views/OfertaDetalle.vue'),
+      props: true
+    },
+     {
+      path: "/perfil",
+      name: "perfil",
+      component: () => import('@/views/Perfil.vue'),
+      props: true
+    },
+          {
+      path: "/dashboard",
+      name: "dashboard",
+      component: () => import('@/views/Dasboard.vue'),
+      props: true
+    },
+  ],
+})
+
+export default router

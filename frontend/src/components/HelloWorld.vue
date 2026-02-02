7aef14c81a0730115f81a09aa70a38baf6e3e670
Scarlett Toala <scarlett.toala.7e8@itb.cat>
2026-02-02 14:54:13 +0000

    VUE

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+import api from '@/services/api';
+
+const { data } = await api.get('/users');
+
+</script>
+
+<template>
+  <h1>Pruebas</h1>
+  
+</template>
+
+<style scoped>
+
+</style>
